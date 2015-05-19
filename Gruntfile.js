ae561f7fc47efff6bfe5834803243872eaf93c7b
mathias.larsen <tkz@topdanmark.dk>
2015-05-19 16:29:28 +0200

    Some stuff

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['connect:server']);
+
+    grunt.initConfig({
+        conf: {
+            app_root: ''
+        },
+        connect: {
+            options: {
+                port: 8002
+            },
+            server: {
+                options: {
+                    open: true,
+                    keepalive: true,
+                    base: '<%= conf.app_root %>'
+                }
+            }
+        }
+    });
+
+};
