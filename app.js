9689639993df47706be7baf1afbcb4b773e5b922
mathias.larsen <tkz@topdanmark.dk>
2015-05-20 09:06:41 +0200

    Awesome stuff

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
+var app = angular.module('StarterApp', ['ngMaterial']).config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+
+        .accentPalette('red');
+
+});
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
     $scope.toggleSidenav = function(menuId) {
