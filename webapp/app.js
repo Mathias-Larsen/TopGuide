ae5eddd04ff2b8d8c633d742384dd29085af04d1
mathias.larsen <tkz@topdanmark.dk>
2015-06-11 12:46:49 +0200

    More stuff

--- webapp/app.js
+++ webapp/app.js
@@ -1,13 +1,40 @@
-var app = angular.module('StarterApp', ['ngMaterial']).config(function($mdThemingProvider) {
+var app = angular.module('StarterApp', ['ngMaterial', 'ui.router']).config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('cyan')
         .accentPalette('red');
 
 });
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/welcome");
+
+    $stateProvider
+        .state('welcome', {
+            url: "/welcome",
+            templateUrl: "views/welcome.html"
+        })
+        .state('icons', {
+            url: "/icons",
+            templateUrl: "views/icons.html"
+        })
+        .state('buttons', {
+            url: "/buttons",
+            templateUrl: "views/buttons.html"
+        })
+        .state('inputfields', {
+            url: "/inputfields",
+            templateUrl: "views/inputfields.html"
+        });
+
+});
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', function($scope, $mdSidenav, $mdToast){
     $scope.toggleSidenav = function(menuId) {
         $mdSidenav(menuId).toggle();
+    };
+    $scope.openToast = function($event) {
+        $mdToast.show($mdToast.simple().content('Hello, i am the roast toast!!'));
     };
 
 }]);
