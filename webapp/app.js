169fac97963f1b153ff032ba607bd0d53394124e
rikke.petersen <rvy@topdanmark.dk>
2015-07-15 13:22:42 +0200

    Playing with toasts

--- webapp/app.js
+++ webapp/app.js
@@ -74,82 +74,84 @@ app.config(function($routeProvider, $locationProvider) {
     });
 });
 
-angular.module('toastDemo1', ['ngMaterial'])
-    .controller('AppCtrl', function($scope, $mdToast, $animate) {
-        $scope.toastPosition = {
-            bottom: false,
-            top: true,
-            left: false,
-            right: true
-        };
-        $scope.getToastPosition = function() {
-            return Object.keys($scope.toastPosition)
-                .filter(function(pos) { return $scope.toastPosition[pos]; })
-                .join(' ');
-        };
-        $scope.showCustomToast = function() {
-            $mdToast.show({
-                controller: 'ToastCtrl',
-                templateUrl: 'toast-template.html',
-                hideDelay: 6000,
-                position: $scope.getToastPosition()
-            });
-        };
-        $scope.showSimpleToast = function() {
-            $mdToast.show(
-                $mdToast.simple()
-                    .content('Simple Toast!')
-                    .position($scope.getToastPosition())
-                    .hideDelay(3000)
-            );
-        };
-        $scope.showActionToast = function() {
-            var toast = $mdToast.simple()
-                .content('Action Toast!')
-                .action('OK')
-                .highlightAction(false)
-                .position($scope.getToastPosition());
-            $mdToast.show(toast).then(function() {
-                alert('You clicked \'OK\'.');
-            });
-        };
-    })
+//Toast function
 
-    .controller('ToastCtrl', function($scope, $mdToast) {
-        $scope.closeToast = function() {
-            $mdToast.hide();
-        };
-    });
+//angular.module('toastDemo1', ['ngMaterial'])
+//    .controller('AppCtrl', function($scope, $mdToast) {
+//        $scope.toastPosition = {
+//            bottom: true,
+//            top: false,
+//            left: true,
+//            right: false
+//        };
+//        $scope.getToastPosition = function() {
+//            return Object.keys($scope.toastPosition)
+//                .filter(function(pos) { return $scope.toastPosition[pos]; })
+//                .join(' ');
+//        };
+//        $scope.showCustomToast = function() {
+//            $mdToast.show({
+//                controller: 'ToastCtrl',
+//                templateUrl: 'toast-template.html',
+//                hideDelay: 6000,
+//                position: $scope.getToastPosition()
+//            });
+//        };
+//        $scope.showSimpleToast = function() {
+//            $mdToast.show(
+//                $mdToast.simple()
+//                    .content('Simple Toast!')
+//                    .position($scope.getToastPosition())
+//                    .hideDelay(3000)
+//            );
+//        };
+//        //$scope.showActionToast = function() {
+//        //    var toast = $mdToast.simple()
+//        //        .content('Action Toast!')
+//        //        .action('OK')
+//        //        .highlightAction(false)
+//        //        .position($scope.getToastPosition());
+//        //    $mdToast.show(toast).then(function() {
+//        //        alert('You clicked \'OK\'.');
+//        //    });
+//        //};
+//    })
+//
+//    .controller('ToastCtrl', function($scope, $mdToast) {
+//        $scope.closeToast = function() {
+//            $mdToast.hide();
+//        };
+//    });
 
-//app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', '$location', function($scope, $mdSidenav, $mdToast, $location){
-//    $scope.toggleSidenav = function(menuId) {
-//        $mdSidenav(menuId).toggle();
-//    };
-//    $scope.openToast = function($event) {
-//        $mdToast.show($mdToast.simple().content('Hello, i am the roast toast!!'));
-//    };
-//
-//    $scope.toggleNavigation = function(siteId) {
-//        $location.path('/' + siteId);
-//    };
-//
-//    $scope.title1 = 'Button';
-//    $scope.title4 = 'Warn';
-//    $scope.isDisabled = true;
-//    $scope.googleUrl = 'http://google.com';
-//
-//    $scope.user = {
-//        title: 'Developer',
-//        email: 'ipsum@lorem.com',
-//        firstName: '',
-//        lastName: '' ,
-//        company: 'Google' ,
-//        address: '1600 Amphitheatre Pkwy' ,
-//        city: 'Mountain View' ,
-//        state: 'CA' ,
-//        biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-//        postalCode : '94043'
-//    };
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', '$location', function($scope, $mdSidenav, $mdToast, $location){
+    $scope.toggleSidenav = function(menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+    $scope.openToast = function($event) {
+        $mdToast.show($mdToast.simple().content('Hello, i am the roast toast!!'));
+    };
+
+    $scope.toggleNavigation = function(siteId) {
+        $location.path('/' + siteId);
+    };
+
+    $scope.title1 = 'Button';
+    $scope.title4 = 'Warn';
+    $scope.isDisabled = true;
+    $scope.googleUrl = 'http://google.com';
+
+    $scope.user = {
+        title: 'Developer',
+        email: 'ipsum@lorem.com',
+        firstName: '',
+        lastName: '' ,
+        company: 'Google' ,
+        address: '1600 Amphitheatre Pkwy' ,
+        city: 'Mountain View' ,
+        state: 'CA' ,
+        biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+        postalCode : '94043'
+    };
 
 
 }]);
