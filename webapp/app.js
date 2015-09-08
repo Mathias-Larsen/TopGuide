b33ef856ee96d9a905ac631de68ee3b597a7a48e
Mathias-Larsen <Mathias.e.larsen@gmail.com>
2015-09-08 21:55:45 +0200

    Now with a sleek svg animation on the front page!

--- webapp/app.js
+++ webapp/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('StarterApp', ['ngMaterial', 'ngRoute', 'ngCookies'])
-    .config(function($mdThemingProvider) {
+    .config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('cyan')
             .primaryPalette('cyan')
             .accentPalette('light-blue');
@@ -54,12 +54,12 @@ var app = angular.module('StarterApp', ['ngMaterial', 'ngRoute', 'ngCookies'])
         $mdThemingProvider.alwaysWatchTheme(true);
     });
 
-app.config(function($mdIconProvider) {
+app.config(function ($mdIconProvider) {
     $mdIconProvider.fontSet('fa', 'fontawesome');
 });
 
 // Refactor this one out
-app.config(function($routeProvider, $locationProvider) {
+app.config(function ($routeProvider, $locationProvider) {
     $routeProvider.otherwise('/welcome');
     $routeProvider
         .when('/welcome', {
@@ -127,21 +127,21 @@ app.config(function($routeProvider, $locationProvider) {
     });
 });
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', '$location', '$cookies', function($scope, $mdSidenav, $mdToast, $location, $cookies){
-    $scope.toggleSidenav = function(menuId) {
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', '$location', '$cookies', function ($scope, $mdSidenav, $mdToast, $location, $cookies) {
+    $scope.toggleSidenav = function (menuId) {
         $mdSidenav(menuId).toggle();
     };
 
-    $scope.openToast = function($event) {
+    $scope.openToast = function ($event) {
         $mdToast.show($mdToast.simple().content('Message sent'));
     };
 
-    $scope.openRoastToast = function($event, message) {
+    $scope.openRoastToast = function ($event, message) {
         $mdToast.show($mdToast.simple().content(message))
     };
 
     // Method for toggling navigation from the left sidebar
-    $scope.toggleNavigation = function(siteId) {
+    $scope.toggleNavigation = function (siteId) {
         $location.path('/' + siteId);
     };
 
@@ -155,20 +155,20 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', '$location', '$co
         title: 'Developer',
         email: 'ipsum@lorem.com',
         firstName: '',
-        lastName: '' ,
-        company: 'Google' ,
-        address: '1600 Amphitheatre Pkwy' ,
-        city: 'Mountain View' ,
-        state: 'CA' ,
+        lastName: '',
+        company: 'Google',
+        address: '1600 Amphitheatre Pkwy',
+        city: 'Mountain View',
+        state: 'CA',
         biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-        postalCode : '94043'
+        postalCode: '94043'
     };
 
     // used to retrieve the possibly saved theme from the client cookie store
     $scope.dynamicTheme = $cookies.get('colorTheme');
 
     // set the theme and store the setting in a cookie
-    $scope.toggleDynamicTheme = function(theme) {
+    $scope.toggleDynamicTheme = function (theme) {
         $scope.dynamicTheme = theme;
         $cookies.put('colorTheme', theme)
     };
@@ -180,7 +180,7 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', '$location', '$co
     angular
         .module('staticChipsDemo', ['ngMaterial'])
         .controller('DemoCtrl', DemoCtrl);
-    function DemoCtrl ($timeout, $q) {
+    function DemoCtrl($timeout, $q) {
         this.chipText = 'Football';
     }
 })();
@@ -189,13 +189,199 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdToast', '$location', '$co
 // Whiteframes as output fields
 angular.module('whiteframeBasicUsage', ['ngMaterial']);
 
-app.directive('ngPrism', [function() {
+app.directive('ngPrism', [function () {
     return {
         restrict: 'A',
-        link: function($scope, element, attrs) {
-            element.ready(function() {
+        link: function ($scope, element, attrs) {
+            element.ready(function () {
                 Prism.highlightElement(element[0]);
             });
         }
     }
 }]);
+
+app.directive('ngSvgAnimation', [function () {
+    return {
+        restrict: 'A',
+        link: function ($scope, element, attrs) {
+            element.ready(function () {
+                var docElem = window.document.documentElement;
+
+                window.requestAnimFrame = function () {
+                    return (
+                        window.requestAnimationFrame ||
+                        window.webkitRequestAnimationFrame ||
+                        window.mozRequestAnimationFrame ||
+                        window.oRequestAnimationFrame ||
+                        window.msRequestAnimationFrame ||
+                        function (/* function */ callback) {
+                            window.setTimeout(callback, 1000 / 60);
+                        }
+                    );
+                }();
+
+                window.cancelAnimFrame = function () {
+                    return (
+                        window.cancelAnimationFrame ||
+                        window.webkitCancelAnimationFrame ||
+                        window.mozCancelAnimationFrame ||
+                        window.oCancelAnimationFrame ||
+                        window.msCancelAnimationFrame ||
+                        function (id) {
+                            window.clearTimeout(id);
+                        }
+                    );
+                }();
+
+                function SVGEl(el) {
+                    this.el = el;
+                    this.image = this.el.previousElementSibling;
+                    this.current_frame = 0;
+                    this.total_frames = 60;
+                    this.path = new Array();
+                    this.length = new Array();
+                    this.handle = 0;
+                    this.init();
+                }
+
+                SVGEl.prototype.init = function () {
+                    var self = this;
+                    [].slice.call(this.el.querySelectorAll('path')).forEach(function (path, i) {
+                        self.path[i] = path;
+                        var l = self.path[i].getTotalLength();
+                        self.length[i] = l;
+                        self.path[i].style.strokeDasharray = l + ' ' + l;
+                        self.path[i].style.strokeDashoffset = l;
+                    });
+                };
+
+                SVGEl.prototype.render = function () {
+                    if (this.rendered) return;
+                    this.rendered = true;
+                    this.draw();
+                };
+
+                SVGEl.prototype.draw = function () {
+                    var self = this,
+                        progress = this.current_frame / this.total_frames;
+                    if (progress > 1) {
+                        window.cancelAnimFrame(this.handle);
+                        this.showImage();
+                    } else {
+                        this.current_frame++;
+                        for (var j = 0, len = this.path.length; j < len; j++) {
+                            this.path[j].style.strokeDashoffset = Math.floor(this.length[j] * (1 - progress));
+                        }
+                        this.handle = window.requestAnimFrame(function () {
+                            self.draw();
+                        });
+                    }
+                };
+
+                SVGEl.prototype.showImage = function () {
+                    classie.add(this.image, 'show');
+                    classie.add(this.el, 'hide');
+                };
+
+                function getViewportH() {
+                    var client = docElem['clientHeight'],
+                        inner = window['innerHeight'];
+
+                    if (client < inner)
+                        return inner;
+                    else
+                        return client;
+                }
+
+                function scrollY() {
+                    return window.pageYOffset || docElem.scrollTop;
+                }
+
+                // http://stackoverflow.com/a/5598797/989439
+                function getOffset(el) {
+                    var offsetTop = 0, offsetLeft = 0;
+                    do {
+                        if (!isNaN(el.offsetTop)) {
+                            offsetTop += el.offsetTop;
+                        }
+                        if (!isNaN(el.offsetLeft)) {
+                            offsetLeft += el.offsetLeft;
+                        }
+                    } while (el = el.offsetParent)
+
+                    return {
+                        top: offsetTop,
+                        left: offsetLeft
+                    };
+                }
+
+                function inViewport(el, h) {
+                    var elH = el.offsetHeight,
+                        scrolled = scrollY(),
+                        viewed = scrolled + getViewportH(),
+                        elTop = getOffset(el).top,
+                        elBottom = elTop + elH,
+                    // if 0, the element is considered in the viewport as soon as it enters.
+                    // if 1, the element is considered in the viewport only when it's fully inside
+                    // value in percentage (1 >= h >= 0)
+                        h = h || 0;
+
+                    return (elTop + elH * h) <= viewed && (elBottom) >= scrolled;
+                }
+
+                function init() {
+                    var svgs = Array.prototype.slice.call(document.querySelectorAll('#main svg')),
+                        svgArr = new Array(),
+                        didScroll = false,
+                        resizeTimeout;
+
+                    // the svgs already shown...
+                    svgs.forEach(function (el, i) {
+                        var svg = new SVGEl(el);
+                        svgArr[i] = svg;
+                        setTimeout(function (el) {
+                            return function () {
+                                if (inViewport(el.parentNode)) {
+                                    svg.render();
+                                }
+                            };
+                        }(el), 250);
+                    });
+
+                    var scrollHandler = function () {
+                            if (!didScroll) {
+                                didScroll = true;
+                                setTimeout(function () {
+                                    scrollPage();
+                                }, 60);
+                            }
+                        },
+                        scrollPage = function () {
+                            svgs.forEach(function (el, i) {
+                                if (inViewport(el.parentNode, 0.5)) {
+                                    svgArr[i].render();
+                                }
+                            });
+                            didScroll = false;
+                        },
+                        resizeHandler = function () {
+                            function delayed() {
+                                scrollPage();
+                                resizeTimeout = null;
+                            }
+
+                            if (resizeTimeout) {
+                                clearTimeout(resizeTimeout);
+                            }
+                            resizeTimeout = setTimeout(delayed, 200);
+                        };
+
+                    window.addEventListener('scroll', scrollHandler, false);
+                    window.addEventListener('resize', resizeHandler, false);
+                }
+
+                init();
+            })
+        }
+    }
+}])
